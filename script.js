c245ae90ecdd75e225e764fd28e36cd7501319e3
Brandon Phillips <brandon.phillips976@gmail.com>
2020-09-14 21:02:31 -0700

    created object with all questions

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = {
+    1: {
+        question: "What is 2+2?",
+        choices: ['fish', 22, 4, 7],
+        correct: 4
+    },
+    2: {
+        question: "If my birthday is the twenty-first of July and I was born in the year 2000, how old am I?",
+        choices: [15, 67, "Don't know don't care", 20],
+        correct: 20
+    },
+    3: {
+        question: "If I have 27 pancakes, I'm 6 feet tall, and the Earth has a circumference of 24,901 miles, what is the color of my hair?",
+        choices: ['blue', 'blonde', 'syrup', 'brown'],
+        correct: 'blonde'
+    },
+    4: {
+        question: "What was Albert Einstein's middle name?",
+        choices: ["He didn't have one", "Lukas", "Finn", "Niclas"],
+        correct: "He didn't have one"
+    },
+    5: {
+        question: "What is my name?",
+        choices: ["Robert", "Downey", "Junior", "Brandon"],
+        correct: "Brandon"
+    }
+}
