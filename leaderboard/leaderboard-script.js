d520216be374a6377ec01eafacc0280a77085193
Brandon Phillips <brandon.phillips976@gmail.com>
2020-09-15 22:00:35 -0700

    began work on leaderboard scripting

--- leaderboard/leaderboard-script.js
+++ leaderboard/leaderboard-script.js
@@ -1,6 +1,16 @@
-var recentUserScore = localStorage.getItem('recentUserScore')
+var rankings = []
 
-function renderLeaders() {
-
+function storeRanks() {
+    for (var i = 0; i < localStorage.length; i++) {
+        if (user != 'recentUserScore') {
+            var score = localStorage.getItem(user)
+            rankings.push([user, score])
+        }
+    }
 }
 
+localStorage.setItem('brp321', 35)
+localStorage.setItem('brandon', 30)
+localStorage.setItem('theman', 20)
+
+storeRanks();
